b18dcde86b41ea96aa6fcc5516a947f184c85b8e
kazi_hoque <kazi.nazmul@bjitacademy.com>
2023-05-19 10:42:08 +0600

    assignment commit
    created a online book library
    
    refs #30059

---
+++ src/main/java/com/bjitacademy/booklibrary/onlinebooklibrary/entity/User.java
@@ -0,0 +1,66 @@
+package com.bjitacademy.booklibrary.onlinebooklibrary.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User implements UserDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    private String address;
+    @ManyToMany
+    private Collection<Role> roles;
+    @Transient
+    private Collection<? extends GrantedAuthority> authorities;
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
+}
---
+++ src/main/java/com/bjitacademy/booklibrary/onlinebooklibrary/model/BookModel.java
@@ -0,0 +1,17 @@
+package com.bjitacademy.booklibrary.onlinebooklibrary.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class BookModel {
+    private String title;
+    private String author;
+    private String genre;
+    private Integer price;
+}
